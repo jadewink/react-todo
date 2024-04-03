d40a7fc92410af4272338fcb7e885906e1d8df0a
Jordyn Jones <minicakes@Minicakes-MacBook-Pro.local>
2024-04-02 20:42:13 -0700

    Create Custom Hook - useSemiPersistentState

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,22 @@ import './App.css'
 import TodoList from './TodoList'
 import AddTodoForm from './AddTodoForm'
 
-function App(item) {
-  // const useStorageState = JSON.parse(localStorage.getItem("savedTodoList"));
-  // const useStorageState = ("savedTodoList", initialState) => {
-    const [todoList, setTodoList] = useState(
-      JSON.parse(localStorage.getItem("savedTodoList"))
-    );
+const useSemiPersistentState = () => {
+  const [todoList, setTodoList] = useState(
+    JSON.parse(localStorage.getItem("savedTodoList"))
+  );
 
-      useEffect(() => {
-      localStorage.setItem("savedTodoList", JSON.stringify(todoList));
-    }, [todoList]);  
-    // const [todoList, setTodoList] = useState([useStorageState]);
+  useEffect(() => {
+    localStorage.setItem("savedTodoList", JSON.stringify(todoList));
+  }, [todoList]); 
+
+  return [todoList, setTodoList]
+};
+
+function App(item) {
+  const [todoList, setTodoList] = useSemiPersistentState(
+    JSON.parse(localStorage.getItem("savedTodoList"))
+  );
 
     function addTodo(newTodo) {
       setTodoList([...todoList, newTodo])
