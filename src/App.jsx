e5e7d253299653115983779bb529d78dc2de10e8
Jordyn Jones <minicakes@Minicakes-MacBook-Pro.local>
2024-04-16 01:18:19 -0700

    Async TodoList is now empty and doesn't persist after refresh

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,25 @@ import AddTodoForm from './AddTodoForm'
 function App(item) {
 
   //check if localStorage is null, if null set state to empty array, if not null set state to existing list
-  const savedTodoList = JSON.parse(localStorage.getItem("savedTodoList"));
-  const [todoList, setTodoList] = useState(savedTodoList === null ? [] : savedTodoList);
+  // const savedTodoList = JSON.parse(localStorage.getItem("savedTodoList"));
+  // const [todoList, setTodoList] = useState(savedTodoList === null ? [] : savedTodoList);
+  const [todoList, setTodoList] = useState([]);
+
+  useEffect(() => {
+    new Promise((resolve, reject) =>
+      setTimeout(
+      () => resolve({ data: { todoList: setTodoList } }),
+      2000
+      )
+    );
+
+    // Inside the function, use your state setter to update the list and pass the todoList from your result object
+    // Promise.then(result => {
+    //   setTodoList(result);
+    // });
+
+    
+  }, []);
 
   useEffect(() => {
     localStorage.setItem("savedTodoList", JSON.stringify(todoList));
