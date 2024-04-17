e74d074a4640ee0abc28b4a0bab7f56f1e951dbc
Jordyn Jones <minicakes@Minicakes-MacBook-Pro.local>
2024-04-17 02:03:08 -0700

    Async list is saved but goes away on refresh

--- src/App.jsx
+++ src/App.jsx
@@ -7,24 +7,37 @@ function App(item) {
 
   //check if localStorage is null, if null set state to empty array, if not null set state to existing list
   // const savedTodoList = JSON.parse(localStorage.getItem("savedTodoList"));
-  // const [todoList, setTodoList] = useState(savedTodoList === null ? [] : savedTodoList);
+  // const [todoList, setTodoList] = useState(savedTodoList === null ? [] : {savedTodoList});
   const [todoList, setTodoList] = useState([]);
+  // const [isLoading, setIsLoading] = useState(false);
 
+  // const getAsyncList = () =>
+  //   new Promise((resolve, reject) =>
+  //     setTimeout(
+  //       () => resolve({ data: { todoList: savedTodoList } }),
+  //       2000
+  //     )
+  //   );
+  
   useEffect(() => {
+    // setIsLoading(true);
+    
     new Promise((resolve, reject) =>
       setTimeout(
-      () => resolve({ data: { todoList: setTodoList } }),
-      2000
-      )
-    );
+        () => resolve({ data: { todoList } }),
+        2000
+      ))
 
-    // Inside the function, use your state setter to update the list and pass the todoList from your result object
-    // Promise.then(result => {
-    //   setTodoList(result);
-    // });
+      // Inside the function, use your state setter to update the list and pass the todoList from your result object
+      .then((result) => {
+        setTodoList(result.data.todoList);
+        // console.log(todoList);
+        // console.log("done");
+        // setIsLoading(false);
+       
+      })
 
-    
-  }, []);
+  });
 
   useEffect(() => {
     localStorage.setItem("savedTodoList", JSON.stringify(todoList));
@@ -41,13 +54,28 @@ function App(item) {
     setTodoList(newtodoList);
   }
 
-  return (
+    return (
       <>
         <h1>Todo List</h1>
         <AddTodoForm name={item} onAddTodo={addTodo} />
         <TodoList todoList={todoList} onRemoveTodo={removeTodo}/>
       </>
     )
+    // return (
+    //   <div>
+    //   ...
+    //   <hr />
+    //   {isLoading ? (
+    //   <p>Loading...</p>
+    //   ) : (
+    //     <>
+    //       <h1>Todo List</h1>
+    //       <AddTodoForm name={item} onAddTodo={addTodo} />
+    //       <TodoList todoList={todoList} onRemoveTodo={removeTodo}/>
+    //     </>
+    //   )}
+    //   </div>
+    // )
   }
 
 export default App
